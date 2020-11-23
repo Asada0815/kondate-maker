97b74e80e91d02173751af3b72728568aaf1b6fc
Asada0815 <asada@posl.ait.kyushu-u.ac.jp>
2020-11-24 00:33:15 +0900

    make header

---
+++ front/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="onClickedNavIcon"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>こんだてめーかー</v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+  export default {
+      methods: {
+          onClickedNavIcon: function() {
+              this.$emit('switch')
+          }
+      }
+  }
+</script>
---
+++ front/src/components/SideBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+    >
+      <!--  -->
+    </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+      props: {
+          drawer: Boolean
+      }
+  }
+</script>
