d4a796237dcc5efdf29d3308b9e75944aecc3939
Asada0815 <asada@posl.ait.kyushu-u.ac.jp>
2020-11-23 16:03:15 +0900

    make env

---
+++ db/sql/1_init_foods.sql
@@ -0,0 +1,8 @@
+create table IF not exists `foods`
+(
+    `id`        INT(10) NOT NULL,
+    `name`      VARCHAR(30) NOT NULL,
+    `price`     INT(10) NOT NULL,
+    `calorie`   INT(10) NOT NULL,
+    PRIMARY KEY(`id`)
+)
---
+++ db/sql/test_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO foods (id,name,price,calorie) VALUES (0, "ご飯", 100, 168);
+INSERT INTO foods (id,name,price,calorie) VALUES (1, "菓子パン", 150, 330);
+INSERT INTO foods (id,name,price,calorie) VALUES (2, "鶏むね肉", 70, 108);
